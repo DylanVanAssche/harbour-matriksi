7d65d83697f8e0386070dee990e5d249ad2b57c0
Rudi Timmermans <rudi.timmer@gmx.us>
2017-12-05 19:34:49 +0100

    Update finish language

--- translations/harbour-matriksi-fi.ts
+++ translations/harbour-matriksi-fi.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1">
+<!DOC TS>
+<TS version="2.1" language="fi">
 <context>
     <name>AboutPage</name>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="18"/>
         <source>About Matriksi</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietoja Matriksista</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="62"/>
         <source>An unofficial Matrix Client for SailfishOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Epävirallinen Matrix asiakasohjelma SailfishOS:lle></translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="71"/>
         <source>Matriksi is an unofficial Matrix Client for SailfishOS and distributed under the GPLv3 license.</source>
-        <translation type="unfinished"></translation>
+        <translation>Matriksi on epävirallinen Matrix asiakasohjelma Sailfish käyttöjärjestelmälle ja sitä levitetään GPLv3 lisenssin alaisena</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="83"/>
         <source>Developers</source>
-        <translation type="unfinished"></translation>
+        <translation>Kehittäjät</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="91"/>
         <source>Donate with Paypal</source>
-        <translation type="unfinished"></translation>
+        <translation>Lahjoita Paypalilla</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="100"/>
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Käännökset</translation>
     </message>
 </context>
 <context>
@@ -39,7 +39,7 @@
     <message>
         <location filename="../qml/pages/ChatPage.qml" line="29"/>
         <source>Message @</source>
-        <translation type="unfinished"></translation>
+        <translation>"Viesti @</translation>
     </message>
 </context>
 <context>
@@ -47,7 +47,7 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="38"/>
         <source>Matriksi</source>
-        <translation type="unfinished"></translation>
+        <translation>Matriksi</translation>
     </message>
 </context>
 <context>
@@ -55,22 +55,22 @@
     <message>
         <location filename="../qml/pages/DevelopersPage.qml" line="18"/>
         <source>Matriksi developers</source>
-        <translation type="unfinished"></translation>
+        <translation>Matriksi kehittäjät</translation>
     </message>
     <message>
         <location filename="../qml/pages/DevelopersPage.qml" line="22"/>
         <source>Developers</source>
-        <translation type="unfinished"></translation>
+        <translation>Kehittäjät</translation>
     </message>
     <message>
         <location filename="../qml/pages/DevelopersPage.qml" line="27"/>
         <source>Previous developer</source>
-        <translation type="unfinished"></translation>
+        <translation>Edelliset kehittäjät</translation>
     </message>
     <message>
         <location filename="../qml/pages/DevelopersPage.qml" line="32"/>
         <source>Contributors</source>
-        <translation type="unfinished"></translation>
+        <translation>Avustajat</translation>
     </message>
 </context>
 <context>
@@ -78,22 +78,22 @@
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="25"/>
         <source>About Matriksi</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietoja Matriksista</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="30"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="37"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Kirjaudu ulos</translation>
     </message>
     <message>
         <location filename="../qml/pages/FirstPage.qml" line="106"/>
         <source>Join room...</source>
-        <translation type="unfinished"></translation>
+        <translation>Liity huoneeseen...</translation>
     </message>
 </context>
 <context>
@@ -101,37 +101,37 @@
     <message>
         <location filename="../qml/pages/Login.qml" line="14"/>
         <source>Please wait loading rooms...</source>
-        <translation type="unfinished"></translation>
+        <translation>Odota. Huoneita ladataan...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Login.qml" line="63"/>
         <source>Matriksi</source>
-        <translation type="unfinished"></translation>
+        <translation>Matriksi</translation>
     </message>
     <message>
         <location filename="../qml/pages/Login.qml" line="72"/>
         <source>User Name or Matrix ID:</source>
-        <translation type="unfinished"></translation>
+        <translation>Käyttäjänimi tai Matrix ID</translation>
     </message>
     <message>
         <location filename="../qml/pages/Login.qml" line="73"/>
         <source>username[:server][:port]</source>
-        <translation type="unfinished"></translation>
+        <translation>käyttäjänimi[:palvelin][:portti]</translation>
     </message>
     <message>
         <location filename="../qml/pages/Login.qml" line="107"/>
         <source>Create an Matrix account</source>
-        <translation type="unfinished"></translation>
+        <translation>Luo Matrix tunnus</translation>
     </message>
     <message>
         <location filename="../qml/pages/Login.qml" line="82"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Salasana</translation>
     </message>
     <message>
         <location filename="../qml/pages/Login.qml" line="91"/>
         <source>https://matrix.org</source>
-        <translation type="unfinished"></translation>
+        <translation>https://matrix.org</translation>
     </message>
 </context>
 <context>
@@ -139,7 +139,7 @@
     <message>
         <location filename="../qml/pages/SecondPage.qml" line="29"/>
         <source>Message @</source>
-        <translation type="unfinished"></translation>
+        <translation>Viesti @</translation>
     </message>
 </context>
 <context>
@@ -147,32 +147,32 @@
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="12"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="14"/>
         <source>Matriksi Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Matriksi asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="18"/>
         <source>Fancy colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Hienot värit</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="19"/>
         <source>Use fancy colors on user names</source>
-        <translation type="unfinished"></translation>
+        <translation>Käytä hienoja värejä käyttäjänimissä</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="29"/>
         <source>Dark background</source>
-        <translation type="unfinished"></translation>
+        <translation>Tumma tausta</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="30"/>
         <source>Use dark background on chat</source>
-        <translation type="unfinished"></translation>
+        <translation>Käytä tummaa taustaa keskusteluissa</translation>
     </message>
 </context>
 <context>
@@ -180,12 +180,12 @@
     <message>
         <location filename="../qml/components/about/ThirdPartyLabel.qml" line="42"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisenssi</translation>
     </message>
     <message>
         <location filename="../qml/components/about/ThirdPartyLabel.qml" line="51"/>
         <source>Project Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Projektisivu</translation>
     </message>
 </context>
 <context>
@@ -193,12 +193,12 @@
     <message>
         <location filename="../qml/pages/ThirdPartyPage.qml" line="26"/>
         <source>Third Party</source>
-        <translation type="unfinished"></translation>
+        <translation>Kolmas osapuoli</translation>
     </message>
     <message>
         <location filename="../qml/pages/ThirdPartyPage.qml" line="32"/>
         <source>The LGPL-2.1 license</source>
-        <translation type="unfinished"></translation>
+        <translation>LGPL-2.1 lisenssi</translation>
     </message>
 </context>
 <context>
@@ -206,7 +206,7 @@
     <message>
         <location filename="../qml/pages/TranslationsPage.qml" line="17"/>
         <source>Translators</source>
-        <translation type="unfinished"></translation>
+        <translation>Kääntäjät</translation>
     </message>
 </context>
 </TS>
